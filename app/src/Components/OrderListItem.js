449eb766d5d8669aa8bc40dcca6372d8ad91cc98
Bogdan.m.i <Bogdan.m.i@yandex.ru>
2020-10-30 01:14:23 +0500

    lesson 6. add cart

---
+++ app/src/Components/OrderListItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import Trash from '../image/trash.svg';
+
+const OrderItemStyled = styled.li`
+	display: flex;
+	margin: 15px 0;
+`;
+
+const ItemName = styled.span`
+	flex-grow: 1;
+`;
+
+const ItemPrice = styled.span`
+	margin: 0 15px 0 30px;
+	min-width: 65px;
+	text-align: right;
+`;
+
+const TrashButton = styled.button`
+	width: 24px;
+	height: 24px;
+	border-color: transparent;
+	background-image: url(${Trash});
+	background-color: transparent;
+	background-position: center;
+	background-size: cover;
+	background-repeat: no-repeat;
+	cursor: pointer;
+`;
+
+export const OrderListItem = () => (
+	<OrderItemStyled>
+		<ItemName>banana</ItemName>
+		<span>2</span>
+		<ItemPrice>750 $</ItemPrice>
+		<TrashButton/>
+	</OrderItemStyled>
+);
